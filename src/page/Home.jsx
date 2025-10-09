e0bd69e6b5e7775ae69aacb71712fc4ede6a1904
samsul alam <samsul7718@gmail.com>
2025-10-09 20:52:48 +0530

    product added and displaying successfully

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 
 const Home = () => {
     const [products,setProducts]=useState([])
+    const Navigate=useNavigate();
+
+    useEffect(()=>{
+        fetch("http://localhost:3000/api/product")
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+        .catch(err=>console.log(err));
+      
+    },[])
+      console.log("product from backend",products);
+      const handleCartProduct=(product)=>{
+         addToCart(product);
+         Navigate('/cart')
+      }
   return (
      <div className='p-4'>
         <div className='flex items-center justify-between gap-80 p-5'>
@@ -20,17 +34,21 @@ const Home = () => {
             <hr />
 
         <ul className='space-y-4'>
-            {products.map((item,index)=>(
+            {products.map((product)=>(
 
             <li 
-            key={index} 
+            key={product.id} 
             className="flex items-center gap-4 border p-2 rounded">
-                <img src="" alt="" />
+                <img src={product.imageUrl} alt="" width="100" 
+                className='object-cover'
+                />
                 <div className='flex-1'>
-                   <h3 className="font-medium">name</h3>
-                   <p>price</p>
+                   <h3 className="font-medium">{product.name}</h3>
+                   <p>{product.price}</p>
                 </div>
-                <button className="bg-violet-400 hover:bg-green-400 text-white px-4 py-2 rounded">Add to cart</button>
+                <button
+                onClick={()=>handleCartProduct(product)}
+                className="bg-violet-400 hover:bg-green-400 text-white px-4 py-2 rounded">Add to cart</button>
             </li>
              ))}
         </ul>
