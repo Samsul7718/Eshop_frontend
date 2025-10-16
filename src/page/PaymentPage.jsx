ae9bbad3cdc6557bf9ff8287d74d33349a956f86
samsul alam <samsul7718@gmail.com>
2025-10-16 22:28:45 +0530

    creating payment div

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const PaymentPage = () => {
   return (
     <div>
-      <div>PaymentPage</div>
+      <div className='text-2xl font-bold'>PaymentPage</div>
+      <div className='font-bold text-gray-600'>Total Amount: $100</div>
     </div>
     
   )
