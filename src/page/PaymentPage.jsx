d6b0beb46479021661e9f93167369394bbe57b1e
samsul alam <samsul7718@gmail.com>
2025-10-16 22:40:19 +0530

    payment page layout created

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const PaymentPage = () => {
   return (
-    <div>
-      <div className='text-2xl font-bold'>PaymentPage</div>
-      <div className='font-bold text-gray-600'>Total Amount: $100</div>
+    <div className='flex flex-col items-center justify-center gap-10 mt-20'>
+      <div className='text-2xl font-bold mb-5'>PaymentPage</div>
+      <div className='flex items-center spaces-between justify-center gap-4'>
+         <div className='font-bold text-gray-600'>Total Amount: $100</div>
       <button className='bg-green-400 p-4 rounded-md shadow-lg'>PayNow</button>
+      </div>
+     
     </div>
     
   )
