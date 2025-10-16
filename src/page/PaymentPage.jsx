bd7c81c74e84def5afa74d3fa4d811134faa0c61
samsul alam <samsul7718@gmail.com>
2025-10-16 22:31:02 +0530

    button created

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -5,6 +5,7 @@ const PaymentPage = () => {
     <div>
       <div className='text-2xl font-bold'>PaymentPage</div>
       <div className='font-bold text-gray-600'>Total Amount: $100</div>
+      <button className='bg-green-400 p-4 rounded-md shadow-lg'>PayNow</button>
     </div>
     
   )
