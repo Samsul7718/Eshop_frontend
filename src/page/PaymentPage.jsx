8c9cc11ff939a1b6516b3bf0c3fabd7f4fa31fc1
samsul alam <samsul7718@gmail.com>
2025-10-18 21:46:51 +0530

    total amount also fetch from cart

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {useCart} from '../context/CartContext'
 
 const PaymentPage = () => {
+  const {cart} = useCart();
+   const totalPrice=cart.reduce((sum,item)=>sum+item.price*(item.qty || 1),0);
   return (
     <div className='flex flex-col items-center justify-center gap-10 mt-20'>
       <div className='text-2xl font-bold mb-5'>PaymentPage</div>
-       <div className='font-bold text-gray-600'>Total Amount: $100</div>
+
+         <div
+         className='font-bold text-gray-600 border p-4'>
+          Total Amount: ${totalPrice}
+          </div>
+   
+     
       <div className='flex items-center spaces-between justify-center gap-8'>
          <button className='bg-yellow-500 text-white rounded-md shadow-md p-3'>Shop More</button>
          <button className='bg-green-400 p-3 rounded-md shadow-lg w-30 h-14'>PayNow</button>
