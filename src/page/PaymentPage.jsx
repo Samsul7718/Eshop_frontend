cb3795bb50e99825449a52ed4c263e9aef7da217
samsul alam <samsul7718@gmail.com>
2025-10-18 21:40:02 +0530

    modifyed button

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -4,10 +4,10 @@ const PaymentPage = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-10 mt-20'>
       <div className='text-2xl font-bold mb-5'>PaymentPage</div>
-      <div className='flex items-center spaces-between justify-center gap-4'>
-         <div className='font-bold text-gray-600'>Total Amount: $100</div>
-         <button className='bg-gray-500 text-white'>Shop More</button>
-      <button className='bg-green-400 p-4 rounded-md shadow-lg'>PayNow</button>
+       <div className='font-bold text-gray-600'>Total Amount: $100</div>
+      <div className='flex items-center spaces-between justify-center gap-8'>
+         <button className='bg-yellow-500 text-white rounded-md shadow-md p-3'>Shop More</button>
+         <button className='bg-green-400 p-3 rounded-md shadow-lg w-30 h-14'>PayNow</button>
       </div>
      
     </div>
