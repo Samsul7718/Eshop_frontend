9fe2fd16c9eee2ff2e44d812e6357e975e1f7587
samsul alam <samsul7718@gmail.com>
2025-10-21 11:05:27 +0530

    working on payment page

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -7,15 +7,28 @@ const PaymentPage = () => {
    const totalPrice=cart.reduce((sum,item)=>sum+item.price*(item.qty || 1),0);
   return (
     <div className='flex flex-col items-center justify-center gap-10 mt-20'>
-      <div className='text-2xl font-bold mb-5'>PaymentPage</div>
+      <h2 className='text-2xl font-bold mb-5'>Payment</h2>
 
          <div
          className='font-bold text-gray-600 border p-4'>
           Total Amount: ${totalPrice}
           </div>
      {console.log('total payment amount',totalPrice)}
+
+     <div className='space-y-2'>
+       {["card","upi","netbanking","wallet"].map((process)=>(
+        <label key={process} className='flex flex-col space-y-1'>
+          <input 
+          name='paymentMethod'
+          value={process}
+          type="radio" />
+          <span className='capitalize'>{process}</span>
+
+        </label>
+       ))}
+     </div>
      
-      <div className='flex items-center spaces-between justify-center gap-8'>
+      {/* <div className='flex items-center spaces-between justify-center gap-8'>
         <Link to='/'>
          <button className='bg-yellow-500 text-white rounded-md shadow-md p-3'>Shop More</button>
         </Link>
@@ -25,7 +38,7 @@ const PaymentPage = () => {
         <Link to='/'>
          <button className='bg-green-400 p-3 rounded-md shadow-lg w-30 h-14'>PayNow</button>
         </Link>
-      </div>
+      </div> */}
      
     </div>
     
