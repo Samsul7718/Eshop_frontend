dde12be0ded803978d164618c527473cae719630
samsul alam <samsul7718@gmail.com>
2025-11-12 20:37:11 +0530

    form created in login page

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -4,6 +4,11 @@ const Login = () => {
   return (
     <div className=''>
       <h2>Login Page</h2>
+      <div>
+        <form action="">
+          <input type="text" placeholder='Enter your Name' />
+        </form>
+      </div>
     </div>
   )
 }
