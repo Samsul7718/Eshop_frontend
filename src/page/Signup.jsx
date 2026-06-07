7afec0141dd916f9940f4bdc7b24d46c50c2a8a5
samsul alam <samsul7718@gmail.com>
2026-06-07 21:33:33 +0530

    signup name input created

--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Signup = () => {
   return (
+    <>
     <h2>Signup</h2>
+    <div>
+     <form>
+      <input type="name" placeholder='Enter your Name' />
+     </form>
+    </div>
+    </>
   )
 }
 
