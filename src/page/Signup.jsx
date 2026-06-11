d2ab8b779152fceebd872e155879669493793414
samsul alam <samsul7718@gmail.com>
2026-06-11 20:55:45 +0530

    mobile authintacation added in user signup

--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -8,6 +8,7 @@ const Signup = () => {
      <form>
       <input type="name" placeholder='Enter your Name' />
       <input type="password" placeholder='Enter your Password' />
+      <input type="mobile" placeholder='Enter your mobile' />
      </form>
     </div>
     </>
